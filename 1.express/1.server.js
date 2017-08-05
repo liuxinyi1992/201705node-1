5df6b759beadae89516a2832ea9473b8c2b4c88e
zhufengnodejs <zhufengnodejs@126.com>
2017-08-05 10:35:39 +0800

    1.安装express

--- 1.express/1.server.js
+++ 1.express/1.server.js
@@ -2,6 +2,14 @@
 let express = require('express');
 //express是一个方法(函数),这个方法可以执行，执行后会返回一个新函数
 let app = express();
+//如何定义路由
+//方法跟http的请求方法是一一对应 GET POST PUT DELETE OPTIONS
+app.get('/',function(req,res){
+  res.end('home');
+});
+app.get('/user',function(req,res){
+  res.end('user');
+});
 //监听8080端口
 app.listen(8080);
 
