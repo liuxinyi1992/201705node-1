95cb1054a4a25317250f1af6e87ffafcd1d06949
zhufengnodejs <zhufengnodejs@126.com>
2017-08-05 13:00:59 +0800

    res.send

--- 1.express/3.中间件/2.ware.js
+++ 1.express/3.中间件/2.ware.js
@@ -12,11 +12,34 @@ app.use(function(req,res,next){
 });
 //我们可以自己写一个res.send方法
 //中间件一个用途就是添加一些公用的方法， 在路由函数里调用
+let STATUS_CODES = {
+  404:'Not Found',
+  500:'Server Error',
+  200:'OK'
+}
 app.use(function(req,res,next){
-  res.s = function(){
-
+  //把body转换成字符串
+  res.s = function(body){
+    let type = typeof body;//获取参数类型
+    switch (type){
+      case 'object'://是一个对象或者数组
+        //把对象或数组转成字符串
+        body = JSON.stringify(body);
+        break;
+      case 'number':
+        //设置响应状态码
+        res.statusCode = body;
+        //设置响应体为此状态码对应的原因短语
+        body = STATUS_CODES[body];
+        break;
+      default:
+        break;
+    }
+    res.end(body);
   }
+  next();
 });
+
 app.get('/users/add',function(req,res){
   //先从查询参数中拿到name的值
   let name = req.query.name;
@@ -26,12 +49,12 @@ app.get('/users/add',function(req,res){
   user.id = users.length>0?users[users.length-1].id+1:1;
   //把新添加的用户添加到用户数组中
   users.push(user);
-  res.end('添加用户成功');
+  res.s('添加用户成功');
 });
 app.get('/users',function(req,res){
   //end只能接收字符串或者Buffer,不能接收对象等其它类型
   //res.send方法可以接收任意类型的参数，包括对象或数组
-  res.send(users);
+  res.s(users);
 });
 app.get('/users/:id',function(req,res){
   //先拿到路径参数中的ID属性
@@ -40,7 +63,10 @@ app.get('/users/:id',function(req,res){
   let user = users.find(function(item){
     return item.id == id;
   });
-  res.send(user);
+  res.s(user);
+});
+app.all('*',function(req,res){
+  res.s(404);
 });
 app.listen(8080);
 
