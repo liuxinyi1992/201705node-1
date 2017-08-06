9de0b870d01f69d4cab4eccb4cc00990b3b067c3
zhufengnodejs <zhufengnodejs@126.com>
2017-08-06 17:50:32 +0800

    res.send

--- 6.express/routes/index.js
+++ 6.express/routes/index.js
@@ -3,6 +3,8 @@ let express = require('express');
 let router = express.Router();
 //定义一个路由，当客户端以GET方式访问/路径的时候，执行对应的回调函数
 router.get('/',function(req,res){
-  res.send('首页');
+  let username = req.cookies.username;
+  let success = req.cookies.success;
+  res.render('index',{username,success});
 });
 module.exports = router;
--- 6.express/routes/user.js
+++ 6.express/routes/user.js
@@ -24,15 +24,19 @@ router.post('/signup', function (req, res) {
   }
 });
 router.get('/signin', function (req, res) {
-  res.render('user/signin', {title: '用户登录'});
+  let error = req.cookies.error;
+  res.render('user/signin', {title: '用户登录',error});
 });
 router.post('/signin', function (req, res) {
   let user = req.body;
   //res.render redirect 都会包含一个 res.end操作
   let oldUser = users.find(item => item.username == user.username && item.password == user.password);
   if (oldUser) {
+    res.cookie('success','恭喜你登录成功');
+    res.cookie('username',user.username);
     res.redirect('/');
   } else {
+    res.cookie('error','你的用户名和密码输入不正确，请重新输入');
     res.redirect('back');
   }
 });
