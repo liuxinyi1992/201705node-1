685a80c0fe476a9d6b9e3a254aabee73d8350875
zhufengnodejs <zhufengnodejs@126.com>
2017-08-06 17:37:49 +0800

    res.send

---
+++ 6.express/routes/user.js
@@ -0,0 +1,39 @@
+let express = require('express');
+let router = express.Router();
+//存放着所有的用户信息
+let users = [];
+router.get('/signup', function (req, res) {
+  //1参数是模板的相对路径，相对于模板根目录的路径
+  //模板的路径一定是一个相对路径
+  let error = req.cookies.error;
+  res.locals.error = error;
+  res.render('user/signup', {title: '用户注册'});
+});
+router.post('/signup', function (req, res) {
+  let user = req.body;// {username,password}
+  let oldUser = users.find(item=>item.username == user.username);
+  if(oldUser){
+    //会把cookie发给客户端，客户端会保存在本地，下次再请求服务器的时候带回服务器
+    res.cookie('error','此用户名已经被占用，请你换个名字吧，比如说'+user.username+'2');
+    res.redirect('back');
+  }else{
+    users.push(user);//把新添加的用户添到数组中去
+    // redirect 重新指向
+    //放的是一个URL绝对路径，一定一定要以/开头
+    res.redirect('/user/signin');
+  }
+});
+router.get('/signin', function (req, res) {
+  res.render('user/signin', {title: '用户登录'});
+});
+router.post('/signin', function (req, res) {
+  let user = req.body;
+  //res.render redirect 都会包含一个 res.end操作
+  let oldUser = users.find(item => item.username == user.username && item.password == user.password);
+  if (oldUser) {
+    res.redirect('/');
+  } else {
+    res.redirect('back');
+  }
+});
+module.exports = router;
---
+++ 6.express/server.js
@@ -0,0 +1,40 @@
+/**
+ * GET /user/signup 获取注册表单
+ * POST /user/signup 提交注册表单
+ * GET /user/signin 获取登录表单
+ * POST /user/signin 提交登录表单
+ * GET / 首页(欢迎页)
+ */
+//express其实是一个函数
+let express = require('express');
+let path = require('path');
+//index提供首页的路由
+let index = require('./routes/index');
+//user提供的是跟用户相关的路由
+let user = require('./routes/user');
+//请求体解析中间件 username=1&password=2
+// {username:1,password:2}
+//基本上所有的第三方中间件都会返回一个函数，执行这个函数才能得到真正的中间件函数
+let bodyParser = require('body-parser');
+let cookieParser = require('cookie-parser');
+//执行后又返回一个函数，请求监听函数
+let app = express();
+app.use(cookieParser());
+//  {"id":1}
+//请求体是什么类型，就用什么解析器来进行解析 不管输入的格式是urlencoded,还是json格式的，输出的格式都是对象
+//extended=true qs querystring
+app.use(bodyParser.urlencoded({extended:true}));
+//经过这个中间件得之后，我们可以通过req.body得到请求体
+//配置模板引擎 html
+app.set('view engine','html');
+//设置模板存放的根目录
+app.set('views',path.resolve('views'));
+//指定html类型的模板的渲染方法
+app.engine('html',require('ejs').__express);
+//express只有亲生的中间件，就是静态文件中间件
+//使用静态文件中间件，指定一个静态文件根目录
+app.use(express.static(path.resolve('../node_modules')));
+app.use('/',index);
+// /user指的是前缀，就是说以/user开头
+app.use('/user',user);
+app.listen(8080);
