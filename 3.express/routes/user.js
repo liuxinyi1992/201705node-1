5d05916043460e61850915313100aa43b6cf6374
zhufengnodejs <zhufengnodejs@126.com>
2017-08-06 10:31:42 +0800

    res.send

--- 3.express/routes/user.js
+++ 3.express/routes/user.js
@@ -1,6 +1,16 @@
 let express = require('express');
 let router = express.Router();
 router.get('/signup',function(req,res){
- res.send('注册');
+  //1参数是模板的相对路径，相对于模板根目录的路径
+  res.render('user/signup',{title:'用户注册'});
+});
+router.post('/signup',function(req,res){
+  res.send('提交注册表单');
+});
+router.get('/signin',function(req,res){
+  res.send('登录');
+});
+router.post('/signin',function(req,res){
+  res.send('提交登录表单');
 });
 module.exports = router;
--- 3.express/server.js
+++ 3.express/server.js
@@ -7,12 +7,20 @@
  */
 //express其实是一个函数
 let express = require('express');
+let path = require('path');
 //index提供首页的路由
 let index = require('./routes/index');
 //user提供的是跟用户相关的路由
 let user = require('./routes/user');
 //执行后又返回一个函数，请求监听函数
 let app = express();
+//配置模板引擎 html
+app.set('view engine','html');
+//设置模板存放的根目录
+app.set('views',path.resolve('views'));
+//指定html类型的模板的渲染方法
+app.engine('html',require('ejs').__express);
+
 app.use('/',index);
 // /user指的是前缀，就是说以/user开头
 app.use('/user',user);
