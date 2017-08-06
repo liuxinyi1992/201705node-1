f024b6dde5581709b667a0ee8829bc903469b369
zhufengnodejs <zhufengnodejs@126.com>
2017-08-06 12:59:37 +0800

    res.send

--- 4.cookie/2.visit.js
+++ 4.cookie/2.visit.js
@@ -3,9 +3,26 @@
  * 当客户端第二次访问的时候，服务器返回 "欢迎你第2次访问"
  */
 let http = require('http');
+let querystring = require('querystring');
+/**
+ * 1.先获取客户端传过来的cookie
+ *
+ */
 http.createServer(function(req,res){
+  res.setHeader('Content-Type','text/html;charset=utf-8');
   if(req.url == '/visit'){
-
+      let cookie = req.headers.cookie;
+      if(cookie){// visit=1
+        //把cookie字符串转成对象
+        let cookieObj = querystring.parse(cookie);
+        let visit = cookieObj.visit;
+        visit = parseInt(visit)+1;
+        res.setHeader('Set-Cookie',`visit=${visit}`);
+        res.end(`欢迎你的第${visit}次访问`);
+      }else{
+        res.setHeader('Set-Cookie','visit=1');
+        res.end(`欢迎你的第1次访问`);
+      }
   }else{
     res.end('404');
   }
