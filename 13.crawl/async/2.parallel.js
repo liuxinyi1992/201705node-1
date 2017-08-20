1bef1c12ed8d7e4c669eada10d09644653162fb3
zhufengnodejs <zhufengnodejs@126.com>
2017-08-20 12:02:04 +0800

    res.send

---
+++ 13.crawl/async/2.parallel.js
@@ -0,0 +1,29 @@
+/**
+ *
+ */
+let async = require('async');
+console.time('cost');
+async.parallel([
+  function(cb){
+    setTimeout(function(){
+      console.log(1);
+      cb(null,'1');
+    },2000)
+  },
+  function(cb){
+    setTimeout(function(){
+      console.log(2);
+      cb(null,'2');
+    },3000)
+  },
+  function(cb){
+    setTimeout(function(){
+      console.log(3);
+      cb(null,'3');
+    },1000)
+  }
+],function(err,result){
+  console.log(err);
+  console.log(result);
+  console.timeEnd('cost');
+});
