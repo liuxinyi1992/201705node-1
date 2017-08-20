8a0fd1ddbfacc1b967ccdd4d084827920de48b23
zhufengnodejs <zhufengnodejs@126.com>
2017-08-20 11:16:57 +0800

    res.send

---
+++ 13.crawl/4.crawl.js
@@ -0,0 +1,14 @@
+let request = require('request');
+let cheerio = require('cheerio');
+let iconv = require('iconv-lite');
+let url = 'http://top.baidu.com/category?c=1';
+//手工强行指定编码为null,这样的话request模块将不再帮我们转换成字符串
+request({url,encoding:null},function(err,response,body){
+  if(!err && response.statusCode == 200){
+    //把GBK格式的buffer转成utf8的字符串
+    body = iconv.decode(body,'gbk');
+    let $ = cheerio.load(body);
+
+  }
+});
+
