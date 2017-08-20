d30d9658f78f9a3e49002813420830899a0ac17c
zhufengnodejs <zhufengnodejs@126.com>
2017-08-20 10:38:07 +0800

    res.send

--- 13.crawl/2.cheerio.js
+++ 13.crawl/2.cheerio.js
@@ -2,14 +2,20 @@
  * cheerio是一个在node里面使用的，类似于jquery的工具
  */
 let cheerio = require('cheerio');
-let html = `<span class="text_1"><ul>
-<li style="overflow:hidden"><a href="/news/marketanalysis/view/1470676.shtml" target="_blank"><span class="six"></span>每周市场动态（2017.8.5-2017.8.11）</a></li><li style="overflow:hidden"><a href="/news/marketanalysis/view/1470147.shtml" target="_blank"><span class="six"></span>2017年7月份猪肉价格走势分析</a></li><li style="overflow:hidden"><a href="/news/marketanalysis/view/1470146.shtml" target="_blank"><span class="six"></span>2017年7月份鸡蛋价格走势分析</a></li><li style="overflow:hidden"><a href="/news/marketanalysis/view/1470112.shtml" target="_blank"><span class="six"></span>每周市场动态（2017.7.29-2017.8.4）</a></li><li style="overflow:hidden"><a href="/news/marketanalysis/view/1469802.shtml" target="_blank"><span class="six"></span>每周市场动态（2017.7.22-2017.7.28）</a></li>		
-        </ul></span>`;
+let html = `<li class="conLi conLiji">
+  <em style="width:100px;text-align:left;padding-left:5px;">芹菜</em>
+  <em style="width:80px;">0.60</em>
+  <em style="width:80px;">0.80</em>
+  <em style="width:80px;">1.00</em>
+  <em style="width:80px;">普通</em>
+  <em style="width:80px;">斤</em>
+  <em style="width:106px;">2017-08-19</em>
+</li>`;
 //把html字符串转成一个jquery对象
 let $ = cheerio.load(html);
 $('li.conLiji em').each(function(index,item){
    //把原生的对象转成jquery对象
-   let $this = $(item);
+   let $this = $(this);
    console.log($this.text());//innerText
 
 });
