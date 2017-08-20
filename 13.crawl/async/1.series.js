672e3a0fa93ed78e579697de71e252d85092bbc0
zhufengnodejs <zhufengnodejs@126.com>
2017-08-20 11:51:00 +0800

    res.send

---
+++ 13.crawl/async/1.series.js
@@ -0,0 +1,30 @@
+/**
+ * 串行执行
+ */
+let async = require('async');
+console.time('cost');
+async.series([
+  function(cb){
+    setTimeout(function(){
+      console.log(1);
+      //异步代码完成后要调用cb,并传入错误对象和返回值
+      cb(null,'1');
+    },2000)
+  },
+  function(cb){
+    setTimeout(function(){
+      console.log(2);
+      cb('我错了','2');
+    },3000)
+  },
+  function(cb){
+    setTimeout(function(){
+      console.log(3);
+      cb(null,'3');
+    },1000)
+  }
+],function(err,result){
+  console.log(err);
+  console.log(result);
+  console.timeEnd('cost');
+});
